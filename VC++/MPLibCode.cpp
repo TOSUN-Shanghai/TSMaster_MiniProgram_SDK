a6687e25575fef62669ad40b26d799e34b9e7fa3
TD01 <TD01@tsingdu.com>
2021-07-14 21:59:22 +0800

    callback function examples added for VC++ SDK

--- VC++/MPLibCode.cpp
+++ VC++/MPLibCode.cpp
@@ -7,14 +7,76 @@
 // Function Prorotypes
 s32 func1(const s32 A1, const s32 A2);
 
-// 主step函数，执行周期 5 ms
-void step(void) { // 周期 = 5 ms
+// Variables defintions
+TMPVarInt NewVariable1;
 
+// Timers defintions
+TMPTimerMS NewTimer1;
+
+// 主step函数，执行周期 500 ms
+void step(void) { // 周期 = 500 ms
+	log("step function every 500ms");
+}
+
+// CAN报文接收事件 "NewOn_CAN_Rx1" 针对标识符 = 0x123 (FD)
+void on_canfd_rx_NewOn_CAN_Rx1(const PCANFD ACANFD) { // 针对标识符 = 0x123 (FD)
+	log("CAN frame 0x123 has been received");
+}
+
+// CAN报文发送成功事件 "NewOn_CAN_Tx1" 针对标识符 = 0x123 (FD)
+void on_canfd_tx_NewOn_CAN_Tx1(const PCANFD ACANFD) { // 针对标识符 = 0x123 (FD)
+	log("CAN frame 0x123 has been transmitted successfully");
+}
+
+// CAN报文预发送事件 "NewOn_CAN_PreTx1" 针对标识符 = 0x123 (FD)
+void on_canfd_pretx_NewOn_CAN_PreTx1(const PCANFD ACANFD) { // 针对标识符 = 0x123 (FD)
+	log("CAN frame 0x123 is being transmitted, you can modify its content before sending out");
+}
+
+// LIN报文接收事件 "NewOn_LIN_Rx1" 针对标识符 = 0x12
+void on_lin_rx_NewOn_LIN_Rx1(const PLIN ALIN) { // 针对标识符 = 0x12
+	log("LIN frame 0x12 has been received");
+}
+
+// LIN报文发送成功事件 "NewOn_LIN_Tx1" 针对标识符 = 0x12
+void on_lin_tx_NewOn_LIN_Tx1(const PLIN ALIN) { // 针对标识符 = 0x12
+	log("LIN frame 0x12 has been transmitted successfully");
+}
+
+// LIN报文预发送事件 "NewOn_LIN_PreTx1" 针对标识符 = 0x12
+void on_lin_pretx_NewOn_LIN_PreTx1(const PLIN ALIN) { // 针对标识符 = 0x12
+	log("LIN frame 0x12 is being transmitted, you can modify its content before sending out");
+}
+
+// 变量变化事件 "NewOn_Var_Change1" 针对变量 "NewVariable1"
+void on_var_change_NewOn_Var_Change1(void) { // 变量 = NewVariable1
+	log("NewVariable1 has been changed to %d", NewVariable1.get());
+}
+
+// 定时器触发事件 "NewOn_Timer1" for Timer NewTimer1
+void on_timer_NewOn_Timer1(void) { // 定时器 = NewTimer1
+	log("Timer 100ms fired");
+}
+
+// 启动事件 "NewOn_Start1"
+void on_start_NewOn_Start1(void) { // 程序启动事件
+	log("TSMaster mini program is starting...");
+	NewTimer1.start();
+}
+
+// 停止事件 "NewOn_Stop1"
+void on_stop_NewOn_Stop1(void) { // 程序停止事件
+	log("TSMaster mini program is stopped");
+}
+
+// 快捷键事件 "NewOn_Shortcut1" 快捷键 = Ctrl+R
+void on_shortcut_NewOn_Shortcut1(const s32 AShortcut) { // 快捷键事件 = Ctrl+R
+	log("You have pressed Ctrl + R short-cut key");
 }
 
 // 自定义函数 "func1"
 s32 func1(const s32 A1, const s32 A2) { // 自定义函数
-
+	log("Custom function is called with result = %d", A1 + A2);
 	return A1 + A2;
 
 }
--- VC++/MPLibCodeExtern.cpp
+++ VC++/MPLibCodeExtern.cpp
@@ -118,12 +118,30 @@ DLLEXPORT s32 __stdcall finalize_miniprogram(void)
 
 // MP library functions definition
 
+// Variables defintions
+extern TMPVarInt NewVariable1;
+
+// Timers defintions
+extern TMPTimerMS NewTimer1;
+
 // Retrieve TSMP abilities
 typedef s32 (__stdcall* TRegTSMasterFunction)(const void* AObj, const char* AFuncType, const char* AFuncName, const char* AData, const void* AFuncPointer, const char* ADescription);
 extern void step(void);
+extern void on_canfd_rx_NewOn_CAN_Rx1(const PCANFD ACANFD);
+extern void on_canfd_tx_NewOn_CAN_Tx1(const PCANFD ACANFD);
+extern void on_canfd_pretx_NewOn_CAN_PreTx1(const PCANFD ACANFD);
+extern void on_lin_rx_NewOn_LIN_Rx1(const PLIN ALIN);
+extern void on_lin_tx_NewOn_LIN_Tx1(const PLIN ALIN);
+extern void on_lin_pretx_NewOn_LIN_PreTx1(const PLIN ALIN);
+extern void on_var_change_NewOn_Var_Change1(void);
+extern void on_timer_NewOn_Timer1(void);
+extern void on_start_NewOn_Start1(void);
+extern void on_stop_NewOn_Stop1(void);
+extern void on_shortcut_NewOn_Shortcut1(const s32 AShortcut);
 // extern your custom function definitions here
 extern s32 func1(const s32 A1, const s32 A2);
 DLLEXPORT s32 __stdcall retrieve_mp_abilities(const void* AObj, const TRegTSMasterFunction AReg) {
+  // struct size check, do not modify ======================================================================================================================================
   #define TSMASTER_VERSION "2021.6.22.581" // do not modify this version as it depends on TSMaster implementation !
   if (!AReg(AObj, "check_mp_internal", "version", TSMASTER_VERSION, 0, "")) return -1;
   if (!AReg(AObj, "check_mp_internal", "struct_size", "struct_size_app", (void *)sizeof(TTSMasterConfiguration), "")) return -1;
@@ -138,8 +156,35 @@ DLLEXPORT s32 __stdcall retrieve_mp_abilities(const void* AObj, const TRegTSMast
   if (!AReg(AObj, "check_mp_internal", "struct_size", "struct_size_TMPVarLIN", (void *)sizeof(TMPVarLIN), "")) return -1;
   if (!AReg(AObj, "check_mp_internal", "struct_size", "struct_size_TLIBTSMapping", (void *)sizeof(TLIBTSMapping), "")) return -1;
   if (!AReg(AObj, "check_mp_internal", "struct_size", "struct_size_TLIBSystemVarDef", (void *)sizeof(TLIBSystemVarDef), "")) return -1;
-  if (!AReg(AObj, "step_function", "step", "5", &step, "")) return -1;
-  // add your custom functions here
+  // step function will run with 500 ms interval ============================================================================================================================
+  if (!AReg(AObj, "step_function", "step", "500", &step, "")) return -1;
+  // Mini program variables will be registered here, 0: integer; 1: double; 2: string; 3: TCAN; 4: TCANFD; 5: TLIN ==========================================================
+  if (!AReg(AObj, "var", "NewVariable1", "0", &NewVariable1, "")) return -1;
+  // Timers can be defined here, 100 means interval = 100ms =================================================================================================================
+  if (!AReg(AObj, "timer", "NewTimer1", "100", &NewTimer1, "")) return -1;
+  // CAN receive done callback will be registered here, 291: identifier; -1: standard frame and 0 means extended frame; -1: can fd frame and 0 means classical can frame ====
+  if (!AReg(AObj, "on_can_rx_callback", "on_canfd_rx_NewOn_CAN_Rx1", "291,-1,-1", &on_canfd_rx_NewOn_CAN_Rx1, "")) return -1;
+  // CAN transmit done callback will be registered here, 291: identifier; -1: standard frame and 0 means extended frame; -1: can fd frame and 0 means classical can frame ===
+  if (!AReg(AObj, "on_can_tx_callback", "on_canfd_tx_NewOn_CAN_Tx1", "291,-1,-1", &on_canfd_tx_NewOn_CAN_Tx1, "")) return -1;
+  // CAN pre-transmit callback will be registered here, 291: identifier; -1: standard frame and 0 means extended frame; -1: can fd frame and 0 means classical can frame ====
+  if (!AReg(AObj, "on_can_pretx_callback", "on_canfd_pretx_NewOn_CAN_PreTx1", "291,-1,-1", &on_canfd_pretx_NewOn_CAN_PreTx1, "")) return -1;
+  // LIN receive done callback will be registered here, 18: identifier ======================================================================================================
+  if (!AReg(AObj, "on_lin_rx_callback", "on_lin_rx_NewOn_LIN_Rx1", "18", &on_lin_rx_NewOn_LIN_Rx1, "")) return -1;
+  // LIN transmit done callback will be registered here, 18: identifier =====================================================================================================
+  if (!AReg(AObj, "on_lin_tx_callback", "on_lin_tx_NewOn_LIN_Tx1", "18", &on_lin_tx_NewOn_LIN_Tx1, "")) return -1;
+  // LIN pre-transmit callback will be registered here, 18: identifier ======================================================================================================
+  if (!AReg(AObj, "on_lin_pretx_callback", "on_lin_pretx_NewOn_LIN_PreTx1", "18", &on_lin_pretx_NewOn_LIN_PreTx1, "")) return -1;
+  // Mini program variable change callback will be registered here ==========================================================================================================
+  if (!AReg(AObj, "on_var_change_callback", "on_var_change_NewOn_Var_Change1", "NewVariable1", &on_var_change_NewOn_Var_Change1, "")) return -1;
+  // Timer overflow callback will be registered here ========================================================================================================================
+  if (!AReg(AObj, "on_timer_callback", "on_timer_NewOn_Timer1", "NewTimer1", &on_timer_NewOn_Timer1, "")) return -1;
+  // Mini program start callback will be registered here ====================================================================================================================
+  if (!AReg(AObj, "on_start_callback", "on_start_NewOn_Start1", "", &on_start_NewOn_Start1, "")) return -1;
+  // Mini program stop callback will be registered here =====================================================================================================================
+  if (!AReg(AObj, "on_stop_callback", "on_stop_NewOn_Stop1", "", &on_stop_NewOn_Stop1, "")) return -1;
+  // Short-cut key callback will be registered here =========================================================================================================================
+  if (!AReg(AObj, "on_shortcut_callback", "on_shortcut_NewOn_Shortcut1", "Ctrl+R", &on_shortcut_NewOn_Shortcut1, "")) return -1;
+  // add your custom functions here =========================================================================================================================================
   if (!AReg(AObj, "on_custom_callback", "func1", "const s32 A1, const s32 A2", &func1, "this is a demo api")) return -1;
   // MP library functions
 
