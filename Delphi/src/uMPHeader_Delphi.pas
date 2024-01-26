6a3c22a4ec1c42f21d3345f07de74c588e4f7dc2
LC03 <freshhope@126.com>
2024-01-26 09:28:24 +0800

    new api: set form caption

--- Delphi/src/uMPHeader_Delphi.pas
+++ Delphi/src/uMPHeader_Delphi.pas
@@ -592,6 +592,7 @@ type
   Ttoggle_mdi_form = function(const AFormCaption: pansichar; const AIsMDI: boolean): s32; stdcall;
   Tget_language_id = function(AId: pInt32): s32; stdcall;
   Tcreate_form = function(const AClassName: pansichar; const AForceCreate: boolean; AFormId: pint64): s32; stdcall;
+  Tset_form_caption = function(const AOldCaption: pansichar; const ANewCaption: pansichar): s32; stdcall;
   // TS_APP_PROTO_END (do not modify this line) ================================
   // hardware settings
   TTSConfigureBaudrateCAN = function(const AIdxChn: integer; const ABaudrateKbps: Single; const AListenOnly: boolean; const AInstallTermResistor120Ohm: Boolean): integer; stdcall;
@@ -1362,7 +1363,8 @@ type
     toggle_mdi_form: Ttoggle_mdi_form;
     get_language_id: Tget_language_id;
     create_form: Tcreate_form;
-    FDummy: array [0..701-1] of s32; // place holders, TS_APP_PROTO_END
+    set_form_caption: Tset_form_caption;
+    FDummy: array [0..700-1] of s32; // place holders, TS_APP_PROTO_END
     function start_log_w_filename(const AFileName: string): s32; cdecl;
     function disconnect(): s32; cdecl;
     procedure terminate_application; cdecl;
